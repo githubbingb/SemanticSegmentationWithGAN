64e44b2bd1ceb9b01b808e80f8e141aa6b817b43
Erwin-fy <erwinwfy@gmail.com>
2017-12-11 14:44:37 +0800

    deeplab

--- 1611.08408/deeplab.py
+++ 1611.08408/deeplab.py
@@ -20,103 +20,101 @@ class Deeplab(nn.Module):
     def __init__(self, n_classes):
         super(Deeplab, self).__init__()
         self.n_classes = n_classes
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        self.features = nn.Sequential(OrderedDict([
+            ('conv1_1', nn.Conv2d(3, 64, kernel_size=3, padding=1)),
+            ('relu1_1', nn.ReLU(False)),
+            ('conv1_2', nn.Conv2d(64, 64, kernel_size=3, padding=1)),
+            ('relu1_2', nn.ReLU(False)),
+            ('pool1', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
 
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            ('conv2_1', nn.Conv2d(64, 128, kernel_size=3, padding=1)),
+            ('relu2_1', nn.ReLU(False)),
+            ('conv2_2', nn.Conv2d(128, 128, kernel_size=3, padding=1)),
+            ('relu2_2', nn.ReLU(False)),
+            ('pool2', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
 
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            ('conv3_1', nn.Conv2d(128, 256, kernel_size=3, padding=1)),
+            ('relu3_1', nn.ReLU(False)),
+            ('conv3_2', nn.Conv2d(256, 256, kernel_size=3, padding=1)),
+            ('relu3_2', nn.ReLU(False)),
+            ('conv3_3', nn.Conv2d(256, 256, kernel_size=3, padding=1)),
+            ('relu3_3', nn.ReLU(False)),
+            ('pool3', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
 
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            ('conv4_1', nn.Conv2d(256, 512, kernel_size=3, padding=1)),
+            ('relu4_1', nn.ReLU(False)),
+            ('conv4_2', nn.Conv2d(512, 512, kernel_size=3, padding=1)),
+            ('relu4_2', nn.ReLU(False)),
+            ('conv4_3', nn.Conv2d(512, 512, kernel_size=3, padding=1)),
+            ('relu4_3', nn.ReLU(False)),
+            ('pool4', nn.MaxPool2d(kernel_size=3, stride=1, padding=1)),
 
-            nn.Conv2d(512, 512, kernel_size=3, padding=2, dilation=2),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=2, dilation=2),
-            nn.ReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=2, dilation=2),
-            nn.ReLU(True),
-            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-
-            nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6),
-            nn.ReLU(True),
-            nn.Dropout(0.5, True),
-
-            nn.Conv2d(1024, 1024, kernel_size=1),
-            nn.ReLU(True),
-            nn.Dropout(0.5, True),
-
-            nn.Conv2d(1024, self.n_classes, kernel_size=1),
-
+            ('conv5_1', nn.Conv2d(512, 512, kernel_size=3, padding=2, dilation=2)),
+            ('relu5_1', nn.ReLU(False)),
+            ('conv5_2', nn.Conv2d(512, 512, kernel_size=3, padding=2, dilation=2)),
+            ('relu5_2', nn.ReLU(False)),
+            ('conv5_3', nn.Conv2d(512, 512, kernel_size=3, padding=2, dilation=2)),
+            ('relu5_3', nn.ReLU(False)),
+            ('pool5', nn.MaxPool2d(kernel_size=3, stride=1, padding=1)),
+            ])
         )
-        # self.classifiers1 = nn.Sequential(
-        #     nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6),
-        #     nn.ReLU(True),
-        #     nn.Dropout(0.5, True),
-        #
-        #     nn.Conv2d(1024, 1024, kernel_size=1),
-        #     nn.ReLU(True),
-        #     nn.Dropout(0.5, True),
-        #
-        #     nn.Conv2d(1024, self.n_classes, kernel_size=1),
-        # )
-        # self.classifiers2 = nn.Sequential(
-        #     nn.Conv2d(512, 1024, kernel_size=3, padding=12, dilation=12),
-        #     nn.ReLU(True),
-        #     nn.Dropout(0.5, True),
-        #
-        #     nn.Conv2d(1024, 1024, kernel_size=1),
-        #     nn.ReLU(True),
-        #     nn.Dropout(0.5, True),
-        #
-        #     nn.Conv2d(1024, self.n_classes, kernel_size=1),
-        # )
-        # self.classifiers3 = nn.Sequential(
-        #     nn.Conv2d(512, 1024, kernel_size=3, padding=18, dilation=18),
-        #     nn.ReLU(True),
-        #     nn.Dropout(0.5, True),
-        #
-        #     nn.Conv2d(1024, 1024, kernel_size=1),
-        #     nn.ReLU(True),
-        #     nn.Dropout(0.5, True),
-        #
-        #     nn.Conv2d(1024, self.n_classes, kernel_size=1),
-        # )
-        # self.classifiers4 = nn.Sequential(
-        #     nn.Conv2d(512, 1024, kernel_size=3, padding=24, dilation=24),
-        #     nn.ReLU(True),
-        #     nn.Dropout(0.5, True),
-        #
-        #     nn.Conv2d(1024, 1024, kernel_size=1),
-        #     nn.ReLU(True),
-        #     nn.Dropout(0.5, True),
-        #
-        #     nn.Conv2d(1024, self.n_classes, kernel_size=1),
-        # )
 
-    def forward(self, x):
-        x = self.features(x)
-        #outputs = self.classifiers1(features) + self.classifiers2(features) + self.classifiers3(features) + self.classifiers4(features)
-        return x
+        self.classifiers1 = nn.Sequential(OrderedDict(
+            [('fc6_1', nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6)),
+             ('relu6_1', nn.ReLU(False)),
+             ('dropout6_1', nn.Dropout(0.5, False)),
+
+             ('fc7_1', nn.Conv2d(1024, 1024, kernel_size=1)),
+             ('relu7_1', nn.ReLU(False)),
+             ('dropout7_1', nn.Dropout(0.5, False)),
+
+             ('fc8_cxr_1', nn.Conv2d(1024, self.n_classes, kernel_size=1)),
+             ])
+        )
+
+        self.classifiers2 = nn.Sequential(OrderedDict(
+            [('fc6_2', nn.Conv2d(512, 1024, kernel_size=3, padding=12, dilation=12)),
+             ('relu6_2', nn.ReLU(False)),
+             ('dropout6_2', nn.Dropout(0.5, False)),
+
+             ('fc7_2', nn.Conv2d(1024, 1024, kernel_size=1)),
+             ('relu7_2', nn.ReLU(False)),
+             ('dropout7_2', nn.Dropout(0.5, False)),
+
+             ('fc8_cxr_2', nn.Conv2d(1024, self.n_classes, kernel_size=1)),
+             ])
+        )
+
+        self.classifiers3 = nn.Sequential(OrderedDict(
+            [('fc6_3', nn.Conv2d(512, 1024, kernel_size=3, padding=16, dilation=16)),
+             ('relu6_3', nn.ReLU(False)),
+             ('dropout6_3', nn.Dropout(0.5, False)),
+
+             ('fc7_3', nn.Conv2d(1024, 1024, kernel_size=1)),
+             ('relu7_3', nn.ReLU(False)),
+             ('dropout7_3', nn.Dropout(0.5, False)),
+
+             ('fc8_cxr_3', nn.Conv2d(1024, self.n_classes, kernel_size=1)),
+             ])
+        )
+
+        self.classifiers4 = nn.Sequential(OrderedDict(
+            [('fc6_4', nn.Conv2d(512, 1024, kernel_size=3, padding=24, dilation=24)),
+             ('relu6_4', nn.ReLU(False)),
+             ('dropout6_4', nn.Dropout(0.5, False)),
+
+             ('fc7_4', nn.Conv2d(1024, 1024, kernel_size=1)),
+             ('relu7_4', nn.ReLU(False)),
+             ('dropout7_4', nn.Dropout(0.5, False)),
+
+             ('fc8_cxr_4', nn.Conv2d(1024, self.n_classes, kernel_size=1)),
+             ])
+        )
+
+    def forward(self, inputs):
+        features = self.features(inputs)
+        outputs = self.classifiers1(features) + self.classifiers2(features) + self.classifiers3(features) + self.classifiers4(features)
+        return outputs
 
 
 def weights_init(m):
@@ -125,54 +123,47 @@ def weights_init(m):
         nn.init.xavier_normal(m.weight.data)
         nn.init.constant(m.bias.data, 0)
 
-def main():
-    imgs = torch.FloatTensor(1, 3, 801, 801)
-    gts = torch.LongTensor(1, 101, 101)
 
+def main():
     reader = Reader('/media/Disk/wangfuyu/data/cxr/801/',
-                    '/media/Disk/wangfuyu/data/cxr/801/trainJM.txt')
+                '/media/Disk/wangfuyu/data/cxr/801/trainJM.txt')
 
     model = Deeplab(2)
     model.apply(weights_init)
 
-    # keys = model.state_dict().keys()
-    #
-    # print model, keys
-    #
-    # pretrain_dict = np.load('/media/Disk/wangfuyu/voc12.npy').item()
-    # print pretrain_dict.keys()
-    #
-    # for key in keys:
-    #     if key in pretrain_dict.keys():
-    #         print key
-    #         model.state_dict()[key] = torch.from_numpy(pretrain_dict[key]).float()
+    keys = model.state_dict().keys()
+
+    print model, keys
+
+    pretrain_dict = np.load('/media/Disk/wangfuyu/voc12.npy').item()
+    print pretrain_dict.keys()
+
+    for key in keys:
+        if key in pretrain_dict.keys():
+            print key
+            model.state_dict()[key] = torch.from_numpy(pretrain_dict[key]).float()
 
     mceLoss = nn.CrossEntropyLoss()
 
     model.cuda()
     mceLoss.cuda()
-    imgs, gts = imgs.cuda(), gts.cuda()
 
     optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=5e-4)
 
     for step in range(10000):
         images, ground_truths = reader.next()
-        # label_onehot = torch.FloatTensor([onehot_encoder(label1.numpy()) for label1 in label])
 
-        imgs.copy_(torch.from_numpy(images))
-        gts.copy_(torch.from_numpy(ground_truths))
-
-        imgsv = Variable(imgs)
-        gtsv = Variable(gts)
+        imgs = Variable(torch.from_numpy(images).float().cuda())
+        gts = Variable(torch.from_numpy(ground_truths).long().cuda())
 
         model.zero_grad()
-        pred_map = model(imgsv)
-        loss = mceLoss(pred_map, gtsv)
+        pred_map = model(imgs)
+        loss = mceLoss(pred_map, gts)
         loss.backward()
         optimizer.step()
 
         print loss
-        
+
         if step % 1000 == 0:
             torch.save(model.state_dict(), 'step_%d.pth' % step)
 
